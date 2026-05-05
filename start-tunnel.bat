33b6a5bdb3837e512b53604c140180a8cfc5ae19
Even <evenji184@gmail.com>
2026-05-05 22:31:31 +0800

    优化脚本

--- start-tunnel.bat
+++ start-tunnel.bat
@@ -1,5 +1,6 @@
 @echo off
 cd /d "%~dp0"
+if exist tunnel.log del tunnel.log
 echo 正在启动 Cloudflare Tunnel...
 echo 启动后请运行 get-tunnel-url.bat 获取公网地址
 cloudflared tunnel --url http://localhost:3000 --logfile tunnel.log
